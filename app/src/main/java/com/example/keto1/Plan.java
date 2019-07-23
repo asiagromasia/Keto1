ca69284c16eb48f7566c4423d61382d3651b4fb6
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-07-23 12:02:02 -0700

    Initial commit

---
+++ app/src/main/java/com/example/keto1/Plan.java
@@ -0,0 +1,15 @@
+package com.example.keto1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Plan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+    }
+    public void getsMondays(){}
+}
