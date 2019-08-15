cd3e90c9281b3c261fd97aff18ec453b4e5b15ad
Asia <Joanna.gromadzka@seattlecentral.edu>
2019-08-15 11:40:40 -0700

    assign fields and create inten to pass values

--- app/src/main/java/com/example/keto1/MainActivity.java
+++ app/src/main/java/com/example/keto1/MainActivity.java
@@ -5,17 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+    private EditText editTextCurrentWeight;
+    private EditText editTextStartDate;
+    private EditText editTextGoalWeight;
+    private EditText editTextGoalDate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        editTextCurrentWeight = findViewById(R.id.editTextCurrentWeight);
+        editTextStartDate = findViewById(R.id.editTextStartDate);
+        editTextGoalWeight = findViewById(R.id.editTextGoalWeight);
+        editTextGoalDate = findViewById(R.id.editTextGoalDate);
     }
 
     public void pressSubmit(View view){
+        Integer currentWeight = editTextCurrentWeight.getInputType();
+        Integer startDate = editTextStartDate.getInputType();
+        Integer goalWeight = editTextGoalWeight.getInputType();
+        Integer goalDate = editTextGoalDate.getInputType();
+
         Intent intent = new Intent(this, Plan.class);
+        intent.putExtra("currentWeight", currentWeight);
+        intent.putExtra("startDate", startDate);
+        intent.putExtra("goalWeight", goalWeight);
+        intent.putExtra("goalDate", goalDate);
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/keto1/Plan.java
+++ app/src/main/java/com/example/keto1/Plan.java
@@ -2,6 +2,7 @@ package com.example.keto1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Plan extends AppCompatActivity {
@@ -9,6 +10,10 @@ public class Plan extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the Intent that started this activity and extract the string playerName
+        Intent intent = getIntent();
+      //  Integer currentWeight = intent.getIntExtra("currentWeight");
+
         setContentView(R.layout.activity_plan);
     }
     public void getsMondays(){}
